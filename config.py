7b3d7caf484e214eef463a8439727a129c6dd4a7
louji852-collab <louji852@gmail.com>
2026-02-21 15:35:05 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# إعدادات الوصول الخاصة بك
+API_TOKEN = '8230820549:AAHrDAWfCNjfaCWQKYDDa_rB1lUGymaKUGk'
+ADMIN_ID = 7755049597
+
+# إعدادات الأداء (السرعة)
+# 100 تعني فحص 100 إيميل في نفس اللحظة
+MAX_WORKERS = 100 
+
+# نظام التنبيهات
+SEND_WELCOME_MSG = True
